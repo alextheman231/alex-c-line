5da90a756733c0ba4865a20b11df743e9f4f14eb
AlexMan123456 <alex.man03888@gmail.com>
2026-01-02 18:57:21 +0000

    Configure ./configs subdirectory

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  clean: true,
-  fixedExtension: false,
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    dts: true,
+    clean: true,
+    fixedExtension: false,
+  },
+  {
+    entry: ["src/configs/index.ts"],
+    outDir: "dist/configs",
+    format: ["esm", "cjs"],
+    dts: true,
+    clean: true,
+    fixedExtension: false,
+  },
+]);
