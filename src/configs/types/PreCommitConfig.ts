cdb2d0f7ddc19a740cc832c25e2c807547eba415
AlexMan123456 <alex.man03888@gmail.com>
2026-01-04 15:45:30 +0000

    Add as const to const object
    This is needed to ensure that the type from that is inferred correctly as 'npm' | 'pnpm'.

--- src/configs/types/PreCommitConfig.ts
+++ src/configs/types/PreCommitConfig.ts
@@ -3,7 +3,7 @@ import type { CreateEnumType } from "@alextheman/utility";
 export const PackageManager = {
   NPM: "npm",
   PNPM: "pnpm",
-};
+} as const;
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type PackageManager = CreateEnumType<typeof PackageManager>;
 
