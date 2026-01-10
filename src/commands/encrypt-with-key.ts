4d797e8681a6e8e45e2dab44fd4a567f3957e93b
AlexMan123456 <alex.man03888@gmail.com>
2026-01-10 16:00:12 +0000

    Use the function from utility to do the encryption logic, and have alex-c-line be the wrapper around it
    The function logic was moved to utility to make reuse across TypeScript projects easier, while the command-line version is nice for convenience in case I ever need to quickly do it or use it in CI.

--- src/commands/encrypt-with-key.ts
+++ src/commands/encrypt-with-key.ts
@@ -1,28 +1,17 @@
 import type { Command } from "commander";
 
-import sodium from "libsodium-wrappers";
+import { encryptWithKey as encryptWithKeyUtility } from "@alextheman/utility";
 
 function encryptWithKey(program: Command) {
   program
     .command("encrypt-with-key")
     .alias("encrypt")
     .description("Encrypt a secret given the public base64 key and the thing you want to encrypt.")
-    .argument("<key>", "The public base64 key to encrypt with")
-    .argument("<value>", "The value to encrypt in plaintext")
-    .action(async (key: string, value: string) => {
+    .argument("<publicKey>", "The public base64 key to encrypt with")
+    .argument("<plaintextValue>", "The value to encrypt in plaintext")
+    .action(async (publicKey: string, plaintextValue: string) => {
       try {
-        await sodium.ready;
-
-        const base64Key = sodium.from_base64(key, sodium.base64_variants.ORIGINAL);
-
-        const encryptedValue = sodium.crypto_box_seal(value, base64Key);
-
-        const encryptedBase64Value = sodium.to_base64(
-          encryptedValue,
-          sodium.base64_variants.ORIGINAL,
-        );
-
-        console.info(encryptedBase64Value);
+        console.info(await encryptWithKeyUtility(publicKey, plaintextValue));
       } catch {
         program.error(
           "Encryption failed. Please double-check that the given key is a valid base 64 string.",
