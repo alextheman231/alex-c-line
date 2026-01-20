68ce1e5482aeeb4b2f042789676a1b57d918f59b
AlexMan123456 <alex.man03888@gmail.com>
2026-01-20 23:45:32 +0000

    Add set-release-status-2 which would be more compatible with the new release directory pattern

---
+++ src/commands/set-release-status-2.ts
@@ -0,0 +1,86 @@
+import type { Command } from "commander";
+
+import { DataError, parseZodSchema, VersionNumber } from "@alextheman/utility";
+import z from "zod";
+
+import { readFile, writeFile } from "node:fs/promises";
+import path from "node:path";
+
+import { ReleaseStatus } from "src/utility/getReleaseNoteTemplateFromMarkdown";
+import isValidReleaseDocument from "src/utility/isValidReleaseDocument";
+
+function setReleaseStatus2(program: Command) {
+  program
+    .command("set-release-status-2")
+    .description(
+      "Change the release status on a given release document initially generated from the `create-release-note` command.",
+    )
+    .argument("<documentPath>", "The path to the document")
+    .argument(
+      "[status]",
+      "The status to set the document to",
+      (rawValue) => {
+        const choice = Object.keys(ReleaseStatus).includes(rawValue.toUpperCase())
+          ? ReleaseStatus[rawValue as keyof typeof ReleaseStatus]
+          : rawValue;
+        return parseZodSchema(
+          z.enum(ReleaseStatus),
+          choice,
+          new DataError(
+            choice,
+            "INVALID_RELEASE_STATUS",
+            'Invalid release status. The release status must be one of "In progress" or "Released"',
+          ),
+        );
+      },
+      ReleaseStatus.RELEASED,
+    )
+    .action(async (documentPath, status) => {
+      const packageInfo = JSON.parse(
+        await readFile(path.join(process.cwd(), "package.json"), "utf-8"),
+      );
+
+      const { name } = parseZodSchema(z.object({ name: z.string() }), packageInfo, () => {
+        program.error(
+          "Invalid package.json - expected package.json to contain a `name` property.",
+          { exitCode: 1, code: "INVALID_PACKAGE_JSON" },
+        );
+      });
+
+      if (!documentPath.endsWith("md")) {
+        program.error("❌ ERROR: Invalid file path. Path must lead to a .md file.", {
+          exitCode: 1,
+          code: "INVALID_FILE_PATH",
+        });
+      }
+
+      const fileBasename = path
+        .basename(documentPath)
+        .split(".")
+        .filter((part) => {
+          return part !== "md";
+        })
+        .join(".");
+      const versionNumber = new VersionNumber(fileBasename);
+
+      const fullDocumentPath = path.join(process.cwd(), documentPath);
+      const initialDocument = await readFile(fullDocumentPath, "utf-8");
+
+      if (!isValidReleaseDocument(name, versionNumber, initialDocument)) {
+        program.error("❌ ERROR: Document does not match a valid release note template.", {
+          exitCode: 1,
+          code: "INVALID_RELEASE_NOTE",
+        });
+      }
+
+      const newDocument = initialDocument.replace(
+        /^\*\*Status\*\*:\s*(.+)$/m,
+        `**Status**: ${status}`,
+      );
+
+      await writeFile(fullDocumentPath, newDocument);
+      console.info(`Setting the status of ${documentPath} to "${status}"`);
+    });
+}
+
+export default setReleaseStatus2;
---
+++ tests/commands/set-release-status-2.test.ts
@@ -0,0 +1,195 @@
+import { kebabToCamel, normaliseIndents, VersionNumber } from "@alextheman/utility";
+import { ExecaError } from "execa";
+import { temporaryDirectoryTask } from "tempy";
+import { describe, expect, test } from "vitest";
+
+import { mkdir, readFile, writeFile } from "node:fs/promises";
+import path from "node:path";
+
+import { createAlexCLineTestClientInDirectory } from "tests/test-clients/alex-c-line-test-client";
+
+import getReleaseNoteTemplate from "src/utility/getReleaseNoteTemplate";
+import getReleaseSummary, { getMajorReleaseSummary } from "src/utility/getReleaseSummary";
+
+import { name, version } from "package.json" with { type: "json" };
+
+describe("set-release-status-2", () => {
+  test.skip("Takes a file path to a valid release note and sets the status to released", async () => {
+    await temporaryDirectoryTask(async (temporaryPath) => {
+      const alexCLineTestClient = createAlexCLineTestClientInDirectory(temporaryPath);
+      const versionNumber = new VersionNumber(version);
+
+      await writeFile(
+        path.join(temporaryPath, "package.json"),
+        JSON.stringify({
+          name,
+          version,
+        }),
+      );
+
+      const { exitCode: createReleaseNoteExitCode } =
+        await alexCLineTestClient("create-release-note-2");
+      expect(createReleaseNoteExitCode).toBe(0);
+
+      const documentPath = path.join(
+        temporaryPath,
+        "docs",
+        "releases",
+        `v${versionNumber.major}`,
+        `v${versionNumber.major}.${versionNumber.minor}`,
+        `${versionNumber}.md`,
+      );
+
+      const fileContentsBeforeWrite = await readFile(documentPath, "utf-8");
+      expect(fileContentsBeforeWrite).toContain(versionNumber.toString());
+      expect(fileContentsBeforeWrite).toContain("**Status**: In progress");
+
+      const { exitCode: setReleaseStatusExitCode } = await alexCLineTestClient(
+        "set-release-status",
+        [path.relative(temporaryPath, documentPath)],
+      );
+      expect(setReleaseStatusExitCode).toBe(0);
+
+      const fileContentsAfterWrite = await readFile(documentPath, "utf-8");
+      expect(fileContentsAfterWrite).toContain("**Status**: Released");
+    });
+  });
+
+  test("Only replaces the first occurrence of the status", async () => {
+    await temporaryDirectoryTask(async (temporaryPath) => {
+      const alexCLineTestClient = createAlexCLineTestClientInDirectory(temporaryPath);
+      const versionNumber = new VersionNumber(version);
+
+      const versionType = new VersionNumber(version).type;
+      const documentPath = path.join(
+        "docs",
+        "releases",
+        `v${versionNumber.major}`,
+        `v${versionNumber.major}.${versionNumber.minor}`,
+        `${versionNumber}.md`,
+      );
+
+      await writeFile(
+        path.join(temporaryPath, "package.json"),
+        JSON.stringify({
+          name,
+          version,
+        }),
+      );
+
+      await mkdir(path.dirname(path.join(temporaryPath, documentPath)), { recursive: true });
+      await writeFile(
+        path.join(temporaryPath, documentPath),
+        getReleaseNoteTemplate(name, new VersionNumber(version), "In progress", {
+          descriptionOfChanges: "**Status**: In progress",
+        }),
+      );
+
+      const { exitCode } = await alexCLineTestClient("set-release-status", [documentPath]);
+      expect(exitCode).toBe(0);
+
+      const fileContentsAfterWrite = await readFile(
+        path.join(temporaryPath, documentPath),
+        "utf-8",
+      );
+      expect(
+        fileContentsAfterWrite.startsWith(normaliseIndents`
+            # v${version} (${kebabToCamel(versionType, { startWithUpper: true })} Release)
+
+            **Status**: Released
+        `),
+      ).toBe(true);
+      expect(fileContentsAfterWrite).toContain("**Status**: In progress");
+    });
+  });
+
+  test.each([
+    ["Completely invalid", "hello"],
+    [
+      "Has version title but not status",
+      normaliseIndents`
+            # v${version} (${kebabToCamel(new VersionNumber(version).type, { startWithUpper: true })} Release)
+        `,
+    ],
+    [
+      "Has version title and status, but status is not in progress",
+      normaliseIndents`
+            # v${version} (${kebabToCamel(new VersionNumber(version).type, { startWithUpper: true })} Release)
+
+            **Status**: Invalid
+        `,
+    ],
+    [
+      "Has version, title and status, but summary is invalid",
+      normaliseIndents`
+        # v${version} (${kebabToCamel(new VersionNumber(version).type, { startWithUpper: true })} Release)
+
+        **Status**: In progress
+
+        Invalid summary (is generally automatically generated and should not be edited)
+    `,
+    ],
+    [
+      "Has version title, status, and valid summary, but no description of changes",
+      normaliseIndents`
+        # v${version} (${kebabToCamel(new VersionNumber(version).type, { startWithUpper: true })} Release)
+
+        **Status**: In progress
+
+        ${getReleaseSummary(name, new VersionNumber(version))}
+    `,
+    ],
+    [
+      "Has version title, status, valid summary, description of changes, but is a major release and has no migration notes",
+      normaliseIndents`
+        # v2.0.0 (Major Release)
+
+        **Status**: In progress
+
+        ${getMajorReleaseSummary(name)}
+
+        ## Description of Changes
+
+        These are breaking changes. Figure out how to fix them yourself.
+    `,
+    ],
+  ])("Does not operate on invalid markdown (%s)", async (_: string, documentContents: string) => {
+    await temporaryDirectoryTask(async (temporaryPath) => {
+      const alexCLineTestClient = createAlexCLineTestClientInDirectory(temporaryPath);
+      const versionNumber = new VersionNumber(version);
+      const documentPath = path.join(
+        "docs",
+        "releases",
+        `v${versionNumber.major}`,
+        `v${versionNumber.major}.${versionNumber.minor}`,
+        `${versionNumber}.md`,
+      );
+
+      await writeFile(
+        path.join(temporaryPath, "package.json"),
+        JSON.stringify({
+          name,
+          version,
+        }),
+      );
+
+      await mkdir(path.dirname(path.join(temporaryPath, documentPath)), { recursive: true });
+      await writeFile(path.join(temporaryPath, documentPath), documentContents);
+
+      try {
+        await alexCLineTestClient("set-release-status", [documentPath]);
+        throw new Error("DID_NOT_THROW");
+      } catch (error) {
+        if (error instanceof ExecaError) {
+          const { stderr: errorMessage, exitCode } = error;
+          expect(exitCode).toBe(1);
+          expect(errorMessage).toBe(
+            "❌ ERROR: Document does not match a valid release note template.",
+          );
+        } else {
+          throw error;
+        }
+      }
+    });
+  });
+});
