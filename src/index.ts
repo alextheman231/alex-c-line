2ec8d659975e079994524ee6cb170ed81b79705c
AlexMan123456 <alex.man03888@gmail.com>
2026-02-08 13:38:51 +0000

    Transform the ExecaErrors into DataErrors
    The ExecaErrors tend to be incredibly verbose, especially compared to our DataErrors. As such, we now simply just catch the ExecaError and show only the most relevant parts.

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,20 @@ import { Command } from "commander";
 import updateNotifier from "update-notifier";
 
 import createCommands from "src/commands";
+import formatError from "src/utility/formatError";
 
 import packageInfo from "package.json" with { type: "json" };
 
 (async () => {
-  const program = new Command();
-  program.name("alex-c-line").description("CLI tool built by Alex").version(packageInfo.version);
+  try {
+    const program = new Command();
+    program.name("alex-c-line").description("CLI tool built by Alex").version(packageInfo.version);
 
-  updateNotifier({ pkg: packageInfo }).notify();
+    updateNotifier({ pkg: packageInfo }).notify();
 
-  createCommands(program);
-  await program.parseAsync(process.argv);
+    createCommands(program);
+    await program.parseAsync(process.argv);
+  } catch (error) {
+    formatError(error);
+  }
 })();
