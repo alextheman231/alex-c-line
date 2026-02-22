318926e89776100e146e3e6228c61f4e89b5539a
AlexMan123456 <alex.man03888@gmail.com>
2026-02-22 18:09:25 +0000

    Refactor directory for update --check command

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -2,8 +2,8 @@ import type { Command } from "commander";
 
 import z from "zod";
 
-import checkUpdate from "src/commands/update/checkUpdate";
 import parseZodSchemaForProgram from "src/utility/miscellaneous/parseZodSchemaForProgram";
+import checkUpdate from "src/utility/updates/checkUpdate";
 
 // Will need this for mutual exclusivity checking with --check and --apply
 const optionsSchema = z.object({
@@ -15,11 +15,12 @@ function update(program: Command) {
     .command("update")
     .description("Handle updates of the currently installed alex-c-line")
     .option("--check", "Check for available updates")
+    .option("--apply", "Apply the latest update")
     .action(async (rawOptions) => {
       const options = parseZodSchemaForProgram(program.error, optionsSchema, rawOptions);
 
       if (options.check) {
-        await checkUpdate(program);
+        await checkUpdate(program, { logSuccess: true });
       } else {
         console.info("Unsupported option. Expected `--check`.");
       }
