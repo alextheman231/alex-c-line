42bbc9274f9a8723173d6aa7f0cf06db98434985
AlexMan123456 <alex.man03888@gmail.com>
2026-03-12 02:38:39 +0000

    Add a command to generate the media files for all Python files in a Manim project
    Since Manim doesn't seem to already have this recursive behaviour, we must wrap around it manually. As of now the Manim CLI only takes one file at a time.

--- src/cli/commands/internal/media/generate.ts
+++ src/cli/commands/internal/media/generate.ts
@@ -17,7 +17,7 @@ function internalMediaGenerate(program: Command) {
           const fullPath = path.join(directory, entry.name);
           if (
             entry.isDirectory() &&
-            ![".git", "node_modules", "__pycache__"].includes(entry.name)
+            ![".git", "node_modules", "__pycache__", ".venv"].includes(entry.name)
           ) {
             await readDirectory(fullPath);
           }
