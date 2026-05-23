d0fd34403e201972c6009e384002953721cb420e
AlexMan123456 <alex.man03888@gmail.com>
2026-05-23 01:18:01 +0100

    Add the ability to set the release note content through the CLI
    This is helpful for automation where we may potentially want to create a GitHub Action that writes a release note and pushes it up in a pull request.

--- tests/helpers/getReleaseSummary.ts
+++ tests/helpers/getReleaseSummary.ts
@@ -1,22 +1,22 @@
 import type { VersionNumber } from "@alextheman/utility";
 
-export function getMajorReleaseSummary(packageName: string) {
-  return `This is a new major release of the \`${packageName}\` package. It has the potential to introduce breaking changes that may require a large amount of refactoring. Please read the below description of changes and migration notes for more information.`;
+export function getMajorReleaseSummary(projectName: string) {
+  return `This is a new major release of the \`${projectName}\` package. It has the potential to introduce breaking changes that may require a large amount of refactoring. Please read the description of changes and migration notes below for more information.`;
 }
 
-export function getMinorReleaseSummary(packageName: string) {
-  return `This is a new minor release of the \`${packageName}\` package. It introduces new features in a backwards-compatible way that should require very little refactoring, if any. Please read below the description of changes.`;
+export function getMinorReleaseSummary(projectName: string) {
+  return `This is a new minor release of the \`${projectName}\` package. It introduces new features and other backwards-compatible changes that should require little to no refactoring. Please read the description of changes below.`;
 }
 
-export function getPatchReleaseSummary(packageName: string) {
-  return `This is a new patch release of the \`${packageName}\` package. It fixes issues with the package in a way that should require no refactoring. Please read below the description of changes.`;
+export function getPatchReleaseSummary(projectName: string) {
+  return `This is a new patch release of the \`${projectName}\` package. It includes small, non-breaking changes and should require no refactoring. Please read the description of changes below.`;
 }
 
-function getReleaseSummary(packageName: string, version: VersionNumber) {
+function getReleaseSummary(projectName: string, version: VersionNumber) {
   return {
-    major: getMajorReleaseSummary(packageName),
-    minor: getMinorReleaseSummary(packageName),
-    patch: getPatchReleaseSummary(packageName),
+    major: getMajorReleaseSummary(projectName),
+    minor: getMinorReleaseSummary(projectName),
+    patch: getPatchReleaseSummary(projectName),
   }[version.type];
 }
 
